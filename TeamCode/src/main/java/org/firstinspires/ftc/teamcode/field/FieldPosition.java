a4ddffe5015210b9f8a314097cce916f1ae0bb00
William Barkoff <william@barkoffusa.com>
2019-09-12 19:56:29 -0400

    Add field position

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/field/FieldPosition.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.field;
+
+/**
+ * Field position shows the position of the robot on the field
+ */
+public enum FieldPosition {
+    /**
+     * Red 1 is the space farther from the audience on the red side
+     */
+    RED_1,
+
+    /**
+     * Red 2 is the space closer to the audience on the red side
+     */
+    RED_2,
+
+    /**
+     * Blue 1 is the space farther from the audience on the blue side
+     */
+    BLUE_1,
+
+    /**
+     * Blue 2 is the space farther from the audience on the blue side
+     */
+    BLUE_2
+}
